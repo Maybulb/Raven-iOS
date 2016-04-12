eb396c0aaa2224de324e5547e8c883cb6f207e06
Josh Trommel <joshtrommel@gmail.com>
2016-04-11 18:37:38 -0700

    Added Carthage and Chameleon

---
+++ README.md
@@ -0,0 +1,4 @@
+Raven
+====
+
+A minimalistic poetry app for iOS. *Only this and nothing more.*
--- Raven/FirstViewController.swift
+++ Raven/FirstViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Chameleon
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var firstView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        firstView.backgroundColor = UIColor.flatNavyBlueColorDark()
     }
 
     override func didReceiveMemoryWarning() {
